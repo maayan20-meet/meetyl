8c83cba8db4b3291b60170c2ba13bb2f48db023c
maayan20-meet <maayan20@meet.mit.edu>
2018-11-04 12:14:28 -0500

    lab 5

--- lab1.py
+++ lab1.py
@@ -1,13 +1,43 @@
+
+#print('Maayan')
+#print('Maayan'*3)
+
+'''
+number1 = 42
+print(number1)
+'''
+'''
+num_list = [2,3,4]
+sum = 0
+
+
+for i in num_list:
+	print(i)
+	print(i*2)
+	sum += i
+print(sum)
+'''
+
+'''
 import turtle
 
 turtle.penup()
 turtle.goto(100,100)
 turtle.pendown()
+turtle.begin_fill()
 turtle.goto(100,-100)
 turtle.goto(-100,-100)
 turtle.goto(-100,100)
 turtle.goto(100,100)
-turtle.begin_fill()
+turtle.end_fill()
 
 
 turtle.mainloop()
+'''
+'''
+import turtle
+
+turtle.circle(100)
+
+turtle.mainloop()
+'''
--- lab2.py
+++ lab2.py
@@ -1,4 +1,45 @@
-number = int(input("What number do you wish to check?"))
+'''
+a = [5,4,6,7,3]
+def list_fun():
+	print(a[0])
+	print(a[-1])
+list_fun()
+'''
+'''
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = []
+
+for i in a:
+ 	if i < 5:
+ 		print(i)
+'''
+
+'''
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+c = []
+
+if len(a) > len(b):
+
+	for i in a:
+		for e in b:
+			if i == e:
+				c.append(i)
+
+else:
+	for i in b:
+		for e in a:
+			if i == e:
+				c.append(i)
+print(c)
+'''
+
+import tkinter as tk
+from tkinter import simpledialog
+
+number = int(simpledialog.askstring("input", "Please give me a number to check for prime!", parent=tk.Tk().withdraw()))
+
+
 
 x = 2
 done = False
@@ -14,3 +55,4 @@ while not done:
 	if x==number:
 		done = True
 		print("your number " + str(number) + " is prime")
+
