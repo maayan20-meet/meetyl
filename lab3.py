03af9faff4ade54f86dac2bfe8780844d6c8d716
maayan20-meet <maayan20@meet.mit.edu>
2018-11-18 12:05:24 -0500

    lab 6

--- lab3.py
+++ lab3.py
@@ -1,8 +1,25 @@
 
 
-'''
+
 import turtle
 
+'''
+forward = 200
+angle = 144
+
+turtle.fd(forward)
+turtle.right(angle)
+turtle.fd(forward)
+turtle.right(angle)
+turtle.fd(forward)
+turtle.right(angle)
+turtle.fd(forward)
+turtle.right(angle)
+turtle.fd(forward)
+turtle.right(angle)
+'''
+
+'''
 turtle.addshape("shape.gif")
 
 turtle.shape("shape.gif")
@@ -19,6 +36,34 @@ turtle.fd(100)
 turtle.right(45)
 turtle.fd(100)
 turtle.end_fill()
+
+turtle.mainloop()
+'''
+'''
+turtle.addshape("bob.gif")
+
+turtle.shape("bob.gif")
+
+x = 0
+
+turtle.speed(0)
+
+
+
+for x in range(360):
+
+	turtle.home()
+	turtle.pendown()
+	turtle.right(x)
+	turtle.fd(100)
+	turtle.right(45)
+	turtle.fd(75)
+	turtle.right(90)
+	turtle.fd(50)
+	turtle.penup()
+
+	
+
 
 turtle.mainloop()
 '''
--- lab6.py
+++ lab6.py
@@ -1,15 +1,136 @@
+import turtle
 from turtle import Turtle
 import random
+#import screen 
+
+
+class Rectangle(Turtle):
+	def __init__(self, width, height):
+
+		Turtle.__init__(self)
+
+		turtle.home()
+		turtle.penup()
+		turtle.hideturtle()
+		turtle.begin_poly()
+		turtle.fd(height)
+		turtle.left(90)
+		turtle.fd(width)
+		turtle.left(90)
+		turtle.fd(height)
+		turtle.left(90)
+		turtle.fd(width)
+		turtle.end_poly()
+		q = turtle.get_poly()
+		turtle.register_shape("custom_rect", q)
+
+		self.shape("custom_rect")
+
+#a = Rectangle(100, 200)
+
+#turtle.mainloop()
+
+
+
+
+turtle.colormode(255)
 
 
 
 class Square(Turtle):
-	def __init__(self, size):
-		self.shape("square")
-		self.size(size)
+	def __init__(self, size, speed):
+		Rectangle.__init__(self, size, size)
+
+		self.size = size
+
+		self.speed = speed
+
 
 	def random_color(self):
-		self.color(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+		
+		R = random.randint(0, 255)
+		G = random.randint(0, 255)
+		B = random.randint(0, 255)
 
-c = Square(10)
-c.random_color()
+		RGB = (R, G, B)
+		print(RGB)
+
+		self.color(RGB)
+
+	def speed(self):
+		self.speed(self.speed)
+
+	def say_size(self):
+		print("My size is " + str(self.size) + "!")
+
+	def move(self):
+
+		while (True):
+
+			self.fd(1)
+
+			postx = turtle.xcor()
+			posty = turtle.ycor()
+
+			if (postx > 50 or postx < -50 or posty > 50 or posty < -50):
+				turtle.left(90)
+				turtle.left(random.randint())
+
+
+bob = Square(10, 20)
+bob.random_color()
+bob.say_size()
+
+bob.move()
+
+
+turtle.mainloop()
+
+
+
+
+'''
+class Hexagon(Turtle):
+	def __init__(self, size):
+		Turtle.__init__(self)
+
+		turtle.home()
+		turtle.penup()
+		turtle.hideturtle()
+		turtle.begin_poly()
+		turtle.fd(size)
+		turtle.left(60)
+		turtle.fd(size)
+		turtle.left(60)
+		turtle.fd(size)
+		turtle.left(60)
+		turtle.fd(size)
+		turtle.left(60)
+		turtle.fd(size)
+		turtle.left(60)
+		turtle.fd(size)
+		turtle.end_poly()
+		p = turtle.get_poly()
+		turtle.register_shape("Hexagon", p)
+
+		self.penup()
+
+		self.shape("Hexagon")
+
+
+
+	def jump(self):
+		self.left(90)
+		self.fd(100)
+
+
+a = Hexagon(20)
+a.jump()
+		
+
+turtle.mainloop()
+
+'''
+
+
+
