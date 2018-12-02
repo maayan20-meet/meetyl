b320a36658e1d7442905539d3bddca03b6000c02
maayan20-meet <maayan20@meet.mit.edu>
2018-12-02 13:02:48 -0500

    lab7

--- lab7.py
+++ lab7.py
@@ -2,67 +2,68 @@ from turtle import *
 import turtle
 import random
 import math
-
+turtle.tracer(0)
+turtle.colormode(255)
+turtle.hideturtle()
 
 class Ball(Turtle):
-	def __init__(self, radius, color, speed):
+	def __init__(self,x, y, radius, color, dx, dy):
 		Turtle.__init__(self)
 		self.shape("circle")
 		self.shapesize(radius/10)
 		self.radius = radius
+		self.dx = dx
+		self.dy = dy
 		self.color(color)
-		self.speed(speed)
 		self.pu()
 
-A = Ball(20, 'red', 1)
-B = Ball(15, 'yellow', 2)
-C = Ball(25, 'green', 1)
-D = Ball(32, 'orange', 2)
+	def move(self, screen_width, screen_height):
+		self.goto(self.xcor()+self.dx, self.ycor()+self.dy)
+		if(self.xcor() > screen_width or self.xcor() < -screen_width):
+			self.dx = -self.dx
+		if(self.ycor() > screen_height or self.ycor() < -screen_height):
+			self.dy = -self.dy
+		
 
-def check_collosion(Ball1, Ball2, Ball3, Ball4):
-	
-	Balls = []
+A = Ball(20, 20, 10, 'black', 0.09, 0.015)
+B = Ball(15, 40, 12, 'black', 0.01, 0.08)
+C = Ball(25, 50, 17, 'black', 0.059, 0.08)
+D = Ball(32, 32, 20, 'black', 0.089, 0.045)
 
-	Balls.append(Ball1)
-	Balls.append(Ball2)
-	Balls.append(Ball3)
-	Balls.append(Ball4)
-
-	
+Balls = []
+Balls.append(A)
+Balls.append(B)
+Balls.append(C)
+Balls.append(D)
+def check_collosion():
 	for i in Balls:
-
 		for j in Balls:
+			if i != j:
+				xi = i.xcor()
+				yi = i.ycor()
 
-			xi = i.xcor()
-			yi = i.ycor()
+				xj = j.xcor()
+				yj = j.ycor()
 
-			xj = j.xcor()
-			yj = j.ycor()
+				DisAB = math.sqrt(math.pow((xi-xj), 2) + math.pow((yi-yj), 2))
 
+				# print(DisAB)
+				if(((i.radius/10 + j.radius/10) >= DisAB)):
+					R = random.randint(0, 255)
+					G = random.randint(0, 255)
+					B = random.randint(0, 255)
 
-			DisAB = math.sqrt(math.pow((xi-xi), 2) + math.pow((yi-yj), 2))
-
-			if ((i != j) and (i.radius + j.radius > DisAB)):
-				if (i.radius < j.radius):
-					i.goto(random.randint(-200, 200), random.randint(-200, 200))
-				elif (j.radius < i.radius):
-					j.goto(random.randint(-200, 200), random.randint(-200, 200))
-
-
-
-def move(Ball):
-
-	
-	if(Ball.xcor() < -200 or Ball.xcor() > 200 or Ball.ycor() < -200 or Ball.ycor() > 200):
-		Ball.left(90 + random.randint(1, 179))
-	else:
-		Ball.fd(1)
-		check_collosion(A, B, C, D)
-
+					RGB = (R, G, B)
+					turtle.bgcolor(RGB)
+		
 while(True):
 
-	move(A)
-
+	A.move(200, 200)
+	B.move(200, 200)
+	C.move(200, 200)
+	D.move(200, 200)
+	check_collosion()
+	turtle.getscreen().update()
 
 
 
